8987a265fdd69cb446e1e32647d3d2f5c77e975a
ifenty <ifenty@gmail.com>
2026-03-12 15:27:17 -0700

    copied v4r5 config to new v4r6

---
+++ ECCOv4 Release 6/extension/code/SLR_CORR_OPTIONS.h
@@ -0,0 +1,27 @@
+C CPP options file for SLR_CORR package
+C Use this file for selecting options within the SLR_CORR package
+
+#ifndef SLR_CORR_OPTIONS_H
+#define SLR_CORR_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_SLR_CORR
+
+C balance sea level rise (each time step by default)
+#define ALLOW_SLR_CORR_BALANCE
+
+C When adjusting GMSL, only adjust precipitation
+C where precipitation is positive
+#define MODIDY_POSITIVE_PRECIP_ONLY
+
+C Instead of using a spatially-invariant precipitation
+C adjustment, apply a scaling factor to precipitation 
+#define SCALE_PRECIP_TO_ADJUST
+
+#endif /* ALLOW_SLR_CORR */
+#endif /* ALLOW_SLR_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
---
+++ ECCOv4 Release 6/extension/code/slr_corr/SLR_CORR_SIZE.h
@@ -0,0 +1,8 @@
+C------------------------------------------------------------------------------|
+C                           SLR_CORR_SIZE.h
+C------------------------------------------------------------------------------|
+
+      INTEGER, PARAMETER :: slrc_max_average_recs = 3650
+      INTEGER, PARAMETER :: slrc_n_obs = 3650
+      INTEGER, PARAMETER :: slrc_est_order = 2
+
